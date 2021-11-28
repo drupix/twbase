a5908ef360b2208e96c592ecbbbc41a57911c886
drupix <enzo@drupal-solutions.ch>
2021-11-28 17:51:53 +0100

    Readme updated

--- postcss.config.js
+++ postcss.config.js
@@ -4,6 +4,7 @@ module.exports = {
     'postcss-import': {},
     tailwindcss: {},
     autoprefixer: {},
+    // Uncomment this to minify the outputed css file
     // cssnano: {
     //   preset: "default",
     // },
